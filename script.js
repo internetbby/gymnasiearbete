958c90f1801342b92a82dc13e0821a01839def7a
Linnea <linnea.alse@elev.ga.ntig.se>
2021-10-10 22:09:40 +0200

    basic js front end

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-$(window).load(function () {
-    $(".trigger_popup_fricc").click(function(){
-       $('..hoverpopup').show();
-    });
-    $('.hoverpopup').click(function(){
-        $('.hoverpopup').hide();
-    });
-    $('.popupclose').click(function(){
-        $('..hoverpopup').hide();
-    });
-});
 
+
+//button introduction 
+
+var buttonIntroductionPage = document.querySelector("#buttonIntroductionPage");
+
+
+
+
+buttonIntroductionPage.addEventListener("click", ()=>{
+    console.log ("click");
+    
+    let introductionPage = document.getElementById("introductionPage");
+
+    var element = introductionPage.style; 
+
+    element.display = "none";
+
+    
+});
