4b2155bb242967a2e336ae2f6db5416a7ef7bd17
Linnea <linnea.alse@elev.ga.ntig.se>
2021-10-14 18:55:13 +0200

    jsFrontPage

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 
 
 //button introduction 
-
 var buttonIntroductionPage = document.querySelector("#buttonIntroductionPage");
+var page1Button = document.querySelector("#pageOneOption1")
+var page2Button = document.querySelector("#pageTwoOption1")
+
+//Add button for every new page in this array
+
+let buttonArray = [buttonIntroductionPage, page1Button, page2Button]
+
+//Add page for every button, should be same length as button array 
+
+let introductionPage = document.getElementById("introductionPage");
+let page1 = document.getElementById("pageOne");
+let page2 = document.getElementById("pageTwo")
+
+let pageArray = [introductionPage, page1, page2]
 
 
+for (let i = 0 ; i < buttonArray.length; i++){
 
+    var elementNextPage = pageArray[i+1].style;
 
-buttonIntroductionPage.addEventListener("click", ()=>{
-    console.log ("click");
+    elementNextPage.display = "none";
+
+    buttonArray[i].addEventListener("click", ()=>{
+        console.log ("click");
     
-    let introductionPage = document.getElementById("introductionPage");
-
-    var element = introductionPage.style; 
-
-    element.display = "none";
-
+        var element = pageArray[i].style; 
     
-});
+        element.display = "none";
+
+        elementNextPage.display = "block";
+
+        console.log(buttonArray[i])
+        
+    });
+
+}
+
