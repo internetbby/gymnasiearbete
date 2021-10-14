1ff25962f205dfc6c1b54771b16c72a93a543367
Linnea <linnea.alse@elev.ga.ntig.se>
2021-10-14 20:00:29 +0200

    failed js

--- script.js
+++ script.js
@@ -7,35 +7,50 @@ var page2Button = document.querySelector("#pageTwoOption1")
 
 //Add button for every new page in this array
 
-let buttonArray = [buttonIntroductionPage, page1Button, page2Button]
+let buttonArray = [buttonIntroductigonPage, page1Button, page2Button]
 
 //Add page for every button, should be same length as button array 
 
 let introductionPage = document.getElementById("introductionPage");
 let page1 = document.getElementById("pageOne");
-let page2 = document.getElementById("pageTwo")
+let page2 = document.getElementById("pageTwo");
 
 let pageArray = [introductionPage, page1, page2]
 
+document.addEventListener('click', function (event){
+    let pageYouAreOn = 0; 
 
-for (let i = 0 ; i < buttonArray.length; i++){
+    console.log("click")
 
-    var elementNextPage = pageArray[i+1].style;
+    if(event.target.matches(".hidden")){
+        console.log("click")
+        pageYouAreOn++;
+        page1.classList.Add("shown")
+        page1.classList.remove ("hidden");
+    }
 
-    elementNextPage.display = "none";
+})
 
+
+//Issue is that when the code plays on load, the for loops play. it should be while youre clicking it is playing
+
+/*for (let i = 0 ; i < buttonArray.length; i++){
+
+
+        console.log(buttonArray[i])
     buttonArray[i].addEventListener("click", ()=>{
         console.log ("click");
     
-        var element = pageArray[i].style; 
-    
-        element.display = "none";
+        if(buttonArray[1].style.display != "none"){
 
-        elementNextPage.display = "block";
+            pageArray[i].style.display = "none"; 
 
-        console.log(buttonArray[i])
+            pageArray[i+1].style.display = "block";
+
+
+        }
         
-    });
+        
+    });*/
 
-}
 
